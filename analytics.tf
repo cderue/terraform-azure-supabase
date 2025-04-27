eb5faf74ed323326567c107e7bdbb26b04b878e2
Cédric Derue <cedric.derue@gmail.com>
2025-04-27 15:13:22 +0200

    Update analytics.tf

--- analytics.tf
+++ analytics.tf
@@ -70,3 +70,18 @@ resource "azurerm_container_app" "analytics" {
       }
     }
   }
+
+  ingress {
+      allow_insecure_connections = false
+      external_enabled           = false
+      target_port                = 4000
+      transport                  = "http"
+    }
+  
+    registry {
+      server   = "docker.io"
+      identity = "System"
+    }
+  
+    depends_on = [azurerm_container_app.db]
+  }
